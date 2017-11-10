b39430de00016f3e03755a5c0d1ab1ee108f5011
C3NZ <epichdd@gmail.com>
2017-11-09 20:13:37 -0500

    Prep for more testing

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from bot.bot import Bot
 
 if __name__ == "__main__":
 	cenz = Bot('data/bot_config.txt')
+	cenz.create_status("random")
 
 	while cenz.is_running():
-		cenz.create_status("random")
+		c
 		cenz.shutdown()
