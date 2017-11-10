9ca1b8b910db23b947181c64d21487cbf3d1c410
C3NZ <epichdd@gmail.com>
2017-11-10 17:31:56 -0500

    removed redundant while loop for now

--- main.py
+++ main.py
@@ -3,7 +3,4 @@ from bot.bot import Bot
 if __name__ == "__main__":
 	cenz = Bot('data/bot_config.txt')
 	cenz.create_status("random")
-
-	while cenz.is_running():
-		c
-		cenz.shutdown()
+	cenz.shutdown()
