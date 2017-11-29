cff36eca74092a7f23ebb5eb9052ca1b855259c6
C3NZ <epichdd@gmail.com>
2017-11-29 02:11:50 -0500

    Finished basic command tests, will add more

--- tests/command_tests.py
+++ tests/command_tests.py
@@ -1,19 +1,38 @@
 import unittest
-from command import Command.commands
-from bot import Bot, api
+from bot.bot import Bot
 
 class CommandTests(unittest.TestCase):
 	"""
-	unit testing all of our functions 
+	unit testing default bot commands
 	"""
 	def setUp(self):
-		self.commands = Command.commands
-		self.cenz_bot = Bot(api)
-		self.test_bot = Bot
+		self.bot= Bot('data/bot_config.txt')
 
-	def test_Command(self):
-		for command in self.commands:
-			print(command)
+	def test_hello_world(self):
+		"""
+		Test the hello world command
+		"""
+		output = self.bot.hello_command()
+		self.assertEqual(output, "Hello, world!")
+		print("Hello world command works")
+
+	def test_about_command(self):
+		output = self.bot.about_command()
+
+		api = self.bot.get_api()
+		bot_info = api.me()
+
+		info_list = []
+		info_list.append('Name: ' + str(bot_info.screen_name) +'\n')
+		info_list.append('Desc: ' + str(bot_info.description) + '\n')
+		info_list.append('Friends: ' + str(bot_info.friends_count) + '\n')
+		info_list.append('Created: ' + str(bot_info.created_at) + '\n')
+		info_list.append('Verified: ' + str(bot_info.verified) + '\n')
+		info_list.append('URL: ' + str(bot_info.url) +'\n')
+		info_list.append('Version: cenz_io-v1.0')
+		
+		self.assertEqual(output, "".join(info_list))
+		print("about command works")
 
 
 if __name__ == '__main__':
