207ac89a5fce47947e0f57bc5f6431b3d2bc6867
C3NZ <epichdd@gmail.com>
2017-12-03 16:39:25 -0500

    Added built in logging util, added commands to the bot itself, added more configurations

--- bot/bot.py
+++ bot/bot.py
@@ -3,6 +3,7 @@ Module for running the cenz_io multifunctional twitter bot
 """
 import tweepy
 from tweepy import TweepError, RateLimitError
+
 import time
 
 from random import randint
@@ -22,52 +23,81 @@ class Bot(object):
 		
 		Variables:
 			(bool) bot_running: Whether the bot is active and running or not
-			(CommandManager) command_man: the commands available to the user
+			(dict) config: Contains configuration settings for the bot
+			(bool) debug_mode: Whether the bot is in debug mode or not
+			(API) api: Tweepy API object
+			(User) me: Twitter user object of the bot profile
 			(int) last_checked_message: id of the last dm received
+			(bool) receiving commands: whether the bot is receiving commands or not
+
 		"""
 
-		#Authenticate with the twitter api
+		self.bot_running = True
 		self.config = self._load_config(config_file)
 		
+
 		auth = tweepy.OAuthHandler(self.config["CONSUMER_KEY"],
 			                       self.config["CONSUMER_SECRET"])
 		
 		auth.set_access_token(self.config["ACCESS_TOKEN"], 
 			                  self.config["ACCESS_TOKEN_SECRET"])
 		
-		self.api = tweepy.API(auth)
-		self.bot_running = True
-		self.message_queue = MessageQueue()
-		self.last_checked_message = self._get_last_dm_id(self.config["LAST_ID"])
-		self.receiving_commands = True
-		self.verified_users = []
+		#setup debug mode if set to true in config
+		if self.config["DEBUG"] == "True":
+			self.debug_mode = True
+		else:
+			self.debug_mode = False
 
-	def _get_last_dm_id(self, id_file):
+
+		self.api = tweepy.API(auth)
+		self.me = self.api.me()
+		self.message_queue = MessageQueue()
+		
+		#Setup command_mode if set to True in config
+		if self.config["COMMAND_MODE"] == "True":
+			self.command_mode = True
+			self.last_checked_message = self._get_last_dm_id()
+			self.verified_users = ["xC3NZ"]
+		else:
+			self.command_mode = False
+
+		self._print_info("Config is up")
+		self._print_info("Debug mode set to true")
+		self._print_info("tweepy API and bot identity setup")
+		self._print_info("receiving commands:" + self.receiving_commands)
+		self._print_info("verified_users are:" + ",".join(verfied_users))
+
+	def _get_last_dm_id(self):
 		"""
 		Retrieves the last checked dm 
 
 		Return:
 			(int) dm_id - integer ID of the last dm received
 		"""
-
-		last_dm_file = open(id_file, 'r')
+		self._print_info("Getting last direct message ID")
+		
+		last_dm_file = open(self.config["ID_FILE"], 'r')
 		dm_id = last_dm_file.readline()
 		last_dm_file.close()
-
+		
+		self._print_info("Last DM received from " + self.config["ID_FILE"] + " With an ID of " + str(dm_id))
+		
 		return dm_id 
 
-	def _write_last_dm_id(self, id_file, id):
+	def _write_last_dm_id(self, id):
 		"""
 		Overwrite the last received dm id to a txt file
 		
 		Params:
 			(int) id - ID of the last direct message checked by 
 		"""
-
-		last_dm_file = open(id_file, 'w')
+		self._print_info("Writing the last dm received to file")
+		
+		last_dm_file = open(self.config["ID_FILE"], 'w')
 		last_dm_file.write(id)
 		last_dm_file.close()
-
+		
+		self._print_info("Finished writing dm id to " + self.config["ID_FILE"])
 		
 	def _load_config(self, config_name):
 		"""
@@ -85,8 +115,23 @@ class Bot(object):
 			config_dict[temp_data[0]] = temp_data[1].rstrip()
 
 		config_file.close()
+		
 		return config_dict
+
+	def _print_info(self, message):
+		"""
+		Print info if debug mode is on
+		"""
+		if self.debug_mode:
+			print("[" + self.me.screenname + "]" + "[INFO]" + message)
 	
+	def _print_error(self, message):
+		"""
+		print errors if debug mode is on
+		"""
+		if self.debug_mode:
+			print("[" + self.me.screenname + "]" + "[ERROR]" + message)
+
 	def hello_command(self):
 		"""
 		Return a simple hello world to the user
@@ -111,6 +156,9 @@ class Bot(object):
 		return "".join(info_list)
 	
 	def eightball_command(self):
+		"""
+		Return a quote from the magical 8ball
+		"""
 		quotes = ["It is certain",
 				  "It is decidedly so",
 			      "Without a doubt",
@@ -136,17 +184,19 @@ class Bot(object):
 
 	def help_command(self, *args):
 		"""
-		Returns help commands that don't need verified users to use them
+		Returns information on how to use non privileged bot commands
 		"""
 		help_dict = {"!hello":"- Returns a simple hello world!",
 					 "!about":"- Returns information about the bot.",
 					 "!8ball":"- Returns a accurate predictions from the magical 8ball"}
+		
 		help_string = []
 		
 		if len(args) == 0:
-			help_string.append("Here are all the commands that I have:")
-			for command_names in help_dict.keys():
-				help_string.append(command_names + help_dict[command_names] + "\n")
+			help_string.append("Here are all the commands that I have:\n")
+			
+			for command_name in help_dict.keys():
+				help_string.append(command_name + help_dict[command_names] + "\n")
 
 			return "".join(help_string)
 		else:
@@ -164,19 +214,21 @@ class Bot(object):
 		message queue
 
 		Returns:
-			A list of direct message objects
+			A list of direct message objects received from the bot
 		"""
 		if self.receiving_commands:
 			try:
 				direct_messages = self.api.direct_messages(since_id=self.last_checked_message)
+				
 				for message in direct_messages:
 					self.message_queue.enqueue_node(Node(message))
 					self.last_checked_message = message.id
-
-				return direct_message
+					self._print_info("Message from '" + message.screen_name + "' with message '" + message.text + "' received.")
+				
+				return direct_messages
 		
-			except RateLimitError as e:
-				print(e)
+			except RateLimitError as rate:
+				self._print_error(rate)
 		else:
 			print("Not receiving commands")
 			
@@ -186,9 +238,32 @@ class Bot(object):
 		try:
 			while self.message_queue.get_node_count() != 0:
 				current_message = self.message_queue.dequeue_node()
-				command_input = ""
-		except:
-			pass
+				user = current_message.screen_name
+				
+				user_message = current_message.text.split()
+				command = user_message[0].lower()
+				command_output = ""
+
+				if not command_input[0].startswith('!'):
+					command_output = "I'm sorry, I only accept commands, which start with ! :("
+				else:
+					if command == "!help":
+						command_output =  self.help_command(user_message[1:])
+					elif command == "!hello":
+						command_output = self.hello_command()
+					elif command == "!8ball":
+						command_output = self.eightball_command()
+					else:
+						command_output = "Sorry, I do not have that have command or you mispelled it :("
+
+				executed_commands.append(self.send_message(user, command_output))
+
+			return execetued_commands
+
+		except RateLimitError as rate:
+			self._print_error(rate)
+			return executed_commands
+
 	def create_status(self, text):
 		"""
 		Update the status of the bot's twitter account
@@ -199,6 +274,7 @@ class Bot(object):
 		Returns:
 			Status object 
 		"""
+		self._print_info("Posting status:" + text)
 		return self.api.update_status(text)
 
 	def send_message(self, recipient, message):
@@ -212,6 +288,7 @@ class Bot(object):
 		Returns:
 			Direct message object
 		"""
+		self._printf_info("Sending message to:" + recipient + "containing the text")
 		return self.api.send_direct_message(recipient, text=message)
 	
 	def update_profile_image(self, filename):
@@ -225,22 +302,22 @@ class Bot(object):
         Returns:
             Twitter User object
 		"""
+		self._print_info("Updated prorfile picture")
 		return self.api.update_profile_image(filename)
 	
+	def get_api(self):
+		return self.api
+
+	def get_me(self):
+		return self.me
+
+	def is_running(self):
+		return self.bot_running
+
 	def shutdown(self):
 		"""
 		Shutdown the bot
 		"""
 		self._write_last_dm_id(self.config["LAST_ID"], self.last_checked_message)
 		self.bot_running = False
-
-	def add_commands(self, *commands):
-		self.command_man.add_commands(*commands)
-
-	def get_api(self):
-		return self.api
-
-	def is_running(self):
-		return self.bot_running
-
-	
+		self._print_info("Bot is now shutting down")
