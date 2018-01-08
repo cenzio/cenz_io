3d520028ca0b2b429ce12ee8bfb51eaab303c7d8
C3NZ <epichdd@gmail.com>
2018-01-07 23:11:44 -0500

    Fixed import and changed status to update

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from bot.bot import Bot
+from bot.bot import TwitterBot
 
 if __name__ == "__main__":
 	cenz = TwitterBot('data/bot_config.txt')
-	cenz.create_status("random")
+	cenz.create_status("yoo")
 	cenz.shutdown()
